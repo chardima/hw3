2aed880ce95b36a99b6d2102fd7ad9feefc492a4
Kyle Calica <kycalica@gmail.com>
2015-10-27 23:10:48 -0700

    seq methods

--- Sequence.java
+++ Sequence.java
@@ -2,21 +2,75 @@ public Sequence extends Element{
 
 
 	private Element[] seq = new Element[];
+	public int length;
+
+	
 	public Sequence{
 		seq = null; 
+		length = 0;
+	}
+	
+	public void print(){
 
-	}   
-	 public void print(){
-
-		for(int i = 0 ; i < seq.length(); i++){
+		for(int i = 0 ; i < this.length(); i++){
 			System.out.println("[" + seq[i].print() +"]");
 		}
 	
 	} 
 
-	public void first(){} 
-	public void rest(){}
-	public void length(){}
-	public void add(){}
-	public void delete(){}
+	public Element first(){
+		
+		return seq[0];
+		
+		
+	} 
+	
+	
+	public Seqence[] rest(){
+		Sequence[] x;
+		for(int i = 1; i < this.length(); i++){
+			x[i-1] = seq[i]; 
+			
+		}
+		
+		return x; 
+	}
+	
+	public void length(){
+		
+			return length; 
+	
+	}
+	
+	public void add(Element ele, int pos){
+		
+		if( pos < 0 || pos > this.length()){
+			System.err.println("Position Does not exsist");
+			System.exit(1);
+		}
+		else if(pos > 0 && pos <= this.length() - 1){
+			
+				for(int i = pos; i < this.length(); i++){
+					Element buffer = seq[i + 1];
+					seq[i+1] = seq[i];
+				}
+				seq[pos] = ele;
+				length++; 
+		}
+		
+		
+		
+	}
+	
+	public void delete(int pos){
+		
+		for(int i = pos; i > this.length()-1; i++){
+					//Element buffer = seq[i - 1];
+					seq[i] = seq[i+1];				
+		}
+			length--; 
+		
+	}
+	
+	
 }
