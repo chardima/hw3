7adeb2e2a4c752b90c6a88af3960875d7f3e26f7
Lawrence Wen Li <lilawren@pc7.cs.ucdavis.edu>
2015-11-01 22:43:08 -0800

    constructor and some things

--- Matrix.java
+++ Matrix.java
@@ -1,8 +1,27 @@
 public class Matrix extends Sequence{
 
-	public Matrix(int rowsize, int colsize){}
+	private LinkedList mat;	
+	int rowsize, colsize;
+
+	public Matrix(int rowsize, int colsize){
+		this.rowsize = rowsize;
+		this.colsize = colsize;
+		
+		for(int i = 1; i <= rowsize; i++){
+			Sequence col_matrix = new Sequence();
+			for(int j = 1; j <= colsize; j++)}
+				col_matrix.add(null, j);
+			}//cols	
+			this.mat.add(col_matrix, i);
+		}//rows
+	}
 	
-	public void Set(int rowsize, int colsize){}
+	public void Set(int rowsize, int colsize){ 
+		
+		for(int i = 0; i < rowsize; i++){
+		
+		}//get row
+	}
 	
 	public int Get(int rowsize, int colsize){}
 	
