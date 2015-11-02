e149275a60ff1c739421421708553e6a7cbe3d05
stcalica <kycalica@gmail.com>
2015-11-01 23:40:19 -0800

    added sum

--- Matrix.java
+++ Matrix.java
@@ -1,7 +1,7 @@
-public class Matrix extends Sequence{
+public class Matrix extends Sequence {
 
-	private LinkedList mat;	
-	int rowsize, colsize;
+
+	public int rowsize, colsize;
 
 	public Matrix(int rowsize, int colsize){
 		this.rowsize = rowsize;
@@ -12,7 +12,7 @@ public class Matrix extends Sequence{
 			for(int j = 1; j <= colsize; j++)}
 				col_matrix.add(null, j);
 			}//cols	
-			this.mat.add(col_matrix, i);
+			this.seq.add(col_matrix, i);
 		}//rows
 	}
 	
@@ -24,8 +24,39 @@ public class Matrix extends Sequence{
 	}
 	
 	public int Get(int rowsize, int colsize){}
+	//A + B = C(sum)
+	public Matrix Sum(matrix mat){
+		//this = A and B = mat
+		//matrix is a linkedlist of sequences
+		SequenceIterator a_row = this.begin(); //start at row 0
+		SequenceIterator b_row() = mat.begin();
 	
-	public Matrix Sum(matrix mat){}
+		SequenceIterator a_col = ((Sequence) a_row.get()).begin(); //returns the node or inner sequence  
+		SequenceIterator b_col = ((Sequence) b_row.get()).begin(); //returns the node or inner sequence
+		Matrix sum = new Matrix(this.rowsize, this.colsize); //the sum of matrix
+	        //for each row and column need to add the element and set them to each other
+		int row = 0; 
+		while(a_row != null || b_row != null){
+			int col = 0;
+			while(a_col != null || b_col != null){
+				MyInteger a = (MyInteger) a_col.get();
+				MyInteger b = (MyInteger) b_col.get();
+				MyInteger c = new MyInteger( a.Get() + b.Get());
+				sum.Set(row, col, c.Get());
+				col++;
+				b_col = b_col.advance();
+				a_col = a_col.advance();
+			}
+			row++; 
+			a_row = a_row.advance();
+			b_row = b_row.advance();  
+			 
+		}
+				
+
+	return sum; 
+
+	}
 
 	public Matrix Product(Matrix mat){}
 
